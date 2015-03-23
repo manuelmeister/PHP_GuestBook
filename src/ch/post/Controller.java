85b48697911f76b2646ce3575ccbad6e99485dd5
speedwhy <lukas.walker@bluewin.ch>
2015-03-23 15:59:30 +0100

    Update @Model, @Controller Model constructor input: username, content

--- src/ch/post/Controller.java
+++ src/ch/post/Controller.java
@@ -1,13 +1,25 @@
 package ch.post;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Controller {
 
-    private Model model;
+    private List<Model> models;
     private View view;
-    private Repository repository;
 
-    public Controller(){
-
+    public Controller(List<Model> models){
+        this.models = models;
+        view = new View();
     }
+    public Controller(){
+        this.models = new ArrayList<Model>();
+        view = new View();
+    }
+
+    public void add(String username, String content){
+        models.add(new Model(username, content));
+    }
+
+
 
 }
