56995db963b0624147f3fb28ae6e14e41eb7a1e0
Manuel Meister <news.manueslworld@gmail.com>
2015-03-24 07:23:48 +0100

    Added root window

--- src/ch/post/Main.java
+++ src/ch/post/Main.java
@@ -9,7 +9,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/view.fxml"));
         primaryStage.show();
     }
 
