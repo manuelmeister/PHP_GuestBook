4696eeab15fb5053ec905939030562751d15e79c
Manuel Meister <news.manueslworld@gmail.com>
2015-03-24 07:22:31 +0100

    Added empty methods

--- src/ch/post/view/View.java
+++ src/ch/post/view/View.java
@@ -1,5 +1,29 @@
 package ch.post.view;
 
+import ch.post.Model;
+
 public class View {
 
+    /**
+     * Adds a post to the UI
+     * @param model
+     */
+    public void addPost(Model model) {
+
+    }
+
+    /**
+     * Displays a information popup
+     * @param message
+     */
+    public void alert(String message) {
+
+    }
+
+    /**
+     * Removes all posts from the View
+     */
+    public void clear() {
+
+    }
 }
