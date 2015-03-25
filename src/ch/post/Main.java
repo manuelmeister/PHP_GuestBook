2dc727959a9ae33d6ed03489359039a1e6283286
Manuel Meister <news.manueslworld@gmail.com>
2015-03-25 14:17:26 +0100

    First functional GUI

--- src/ch/post/Main.java
+++ src/ch/post/Main.java
@@ -1,8 +1,9 @@
 package ch.post;
-//hello my friends :D
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -10,6 +11,13 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("view/view.fxml"));
+        String css = this.getClass().getResource("view/style.css").toExternalForm();
+
+        primaryStage.setTitle("GuestBook");
+        Scene scene = new Scene(root, 768, 768);
+
+        scene.getStylesheets().add(css);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
--- src/ch/post/view/View.java
+++ src/ch/post/view/View.java
@@ -2,7 +2,33 @@ package ch.post.view;
 
 import ch.post.Model;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
 public class View {
+
+    @FXML private ScrollPane container;
+
+    @FXML private VBox posts;
+
+    @FXML private TextArea content;
+    @FXML private TextField username;
+    @FXML private Button submit;
+
+    @FXML protected void submitButtonAction(MouseEvent mouseEvent){
+        Date date = new Date();
+        DateFormat dateFormat = new SimpleDateFormat("dd.mm.yyyy HH:mm");
+        posts.getChildren().add(Elements.Post(content.getText(), username.getText(), dateFormat.format(date) ) );
+        content.setText("");
+        username.setText("");
+    }
 
     /**
      * Adds a post to the UI
